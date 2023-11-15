12e8d4f4c4ccb2fa4195dd3749f583b7fc4a01e4
LuZhang907 <63075429+LuZhang907@users.noreply.github.com>
2023-11-15 16:17:56 -0600

    updates

--- Code/stepwise_linear_regression_modeling.R
+++ Code/stepwise_linear_regression_modeling.R
@@ -14,10 +14,6 @@ dim(dat)
 min.model <- lm(r13~1,data = dat)
 max.model <- lm(r13~.,data = dat)
 
-summary(max.model)
-
-vif(max.model)
-
 fwd.model <- step(min.model, direction = "forward",scope = max.model)
 bwd.model <- step(max.model, direction = "backward",scope = min.model)
 both.model <- step(max.model, direction = "both",scope = min.model)
@@ -25,3 +21,6 @@ both.model <- step(max.model, direction = "both",scope = min.model)
 summary(fwd.model)
 summary(bwd.model)
 summary(both.model)
+
+vif(bwd.model)
+vif(both.model)
