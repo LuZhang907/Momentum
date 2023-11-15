4d051c177f9c2d7dcbc6528eb6e900ab171a0af9
LuZhang907 <63075429+LuZhang907@users.noreply.github.com>
2023-11-15 16:16:58 -0600

    updates

---
+++ Code/stepwise_linear_regression_modeling.R
@@ -0,0 +1,27 @@
+rm(list = setdiff(ls(),lsf.str()))
+library(data.table)
+library(tidyr)
+library(car)
+
+dat <- fread("./Data/returns_spy.csv")
+dat <- data.frame(dat)
+dat$date <- NULL
+dat <- dat %>% drop_na()
+
+head(dat)
+dim(dat)
+
+min.model <- lm(r13~1,data = dat)
+max.model <- lm(r13~.,data = dat)
+
+summary(max.model)
+
+vif(max.model)
+
+fwd.model <- step(min.model, direction = "forward",scope = max.model)
+bwd.model <- step(max.model, direction = "backward",scope = min.model)
+both.model <- step(max.model, direction = "both",scope = min.model)
+
+summary(fwd.model)
+summary(bwd.model)
+summary(both.model)
