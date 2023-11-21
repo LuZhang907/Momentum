d17c769eecdb6500d0d03221b08df7af9d9ccee9
LuZhang907 <63075429+LuZhang907@users.noreply.github.com>
2023-11-20 22:38:03 -0500

    updates

--- Code/rolling_returns_calculation.R
+++ Code/rolling_returns_calculation.R
@@ -73,13 +73,13 @@ head(rst)
 # add date
 rst$date <- data[data$minutes == 605,]$date
 
-# input r_LH_lag, r_on, and R_LH
+# input r_LH_lag, r_on, r_FH(9:31-9:35),and R_LH
 rs3 <- fread("./Data/returns_spy.csv")
 rs3 <- data.frame(rs3)
 head(rs3)
 
-rs3 <- rs3 %>% select(date, r13_lag, r_on, r13)
-names(rs3) <- c("date","r_LH_lag","r_on","r_LH")
+rs3 <- rs3 %>% select(date, r13_lag, r_on,r1, r13)
+names(rs3) <- c("date","r_LH_lag","r_on","r_FH","r_LH")
 head(rs3)
 
 returns_all <- merge(rst, rs3, by = "date")
@@ -87,9 +87,9 @@ dim(returns_all)
 
 write.csv(returns_all,"./Data/rolling_returns.csv",row.names = FALSE)
 
-days <- unique(dat$date)
-missing <- ifelse(days%in%dropdays,1,0)
-missing_days <- data.frame(days, missing)
-head(missing_days)
+#days <- unique(dat$date)
+#missing <- ifelse(days%in%dropdays,1,0)
+#missing_days <- data.frame(days, missing)
+#head(missing_days)
 
-write.csv(missing_days,"./Data/missing_day_record.csv",row.names = FALSE)
+#write.csv(missing_days,"./Data/missing_day_record.csv",row.names = FALSE)
