d68c85ffcce9cfca4c19bf988e44d3b9af784326
LuZhang907 <63075429+LuZhang907@users.noreply.github.com>
2023-12-18 11:35:37 -0600

    data prepartion for regime splitting by threshold(overnight return and volatility).

--- Code/iteration_matrix_generating.cpp
+++ Code/iteration_matrix_generating.cpp
@@ -14,7 +14,6 @@ NumericMatrix iter_r1(Rcpp::DataFrame dat, int window){
   return r1_matrix;
 }
 
-
 // [[Rcpp::export]]
 NumericMatrix iter_r2(Rcpp::DataFrame dat, int window){
   NumericVector r2 = dat["r2"];
@@ -67,6 +66,7 @@ NumericMatrix iter_r5(Rcpp::DataFrame dat, int window){
   return r5_matrix;
 }
 
+
 // [[Rcpp::export]]
 NumericMatrix iter_r6(Rcpp::DataFrame dat, int window){
   NumericVector r6 = dat["r6"];
@@ -79,6 +79,7 @@ NumericMatrix iter_r6(Rcpp::DataFrame dat, int window){
   }
   return r6_matrix;
 }
+
 
 // [[Rcpp::export]]
 NumericMatrix iter_r7(Rcpp::DataFrame dat, int window){
@@ -106,6 +107,7 @@ NumericMatrix iter_r8(Rcpp::DataFrame dat, int window){
   return r8_matrix;
 }
 
+
 // [[Rcpp::export]]
 NumericMatrix iter_r9(Rcpp::DataFrame dat, int window){
   NumericVector r9 = dat["r9"];
@@ -118,6 +120,8 @@ NumericMatrix iter_r9(Rcpp::DataFrame dat, int window){
   }
   return r9_matrix;
 }
+
+
 
 // [[Rcpp::export]]
 NumericMatrix iter_r10(Rcpp::DataFrame dat, int window){
@@ -132,6 +136,7 @@ NumericMatrix iter_r10(Rcpp::DataFrame dat, int window){
   return r10_matrix;
 }
 
+
 // [[Rcpp::export]]
 NumericMatrix iter_r11(Rcpp::DataFrame dat, int window){
   NumericVector r11 = dat["r11"];
@@ -144,6 +149,7 @@ NumericMatrix iter_r11(Rcpp::DataFrame dat, int window){
   }
   return r11_matrix;
 }
+
 
 // [[Rcpp::export]]
 NumericMatrix iter_r12(Rcpp::DataFrame dat, int window){
@@ -158,6 +164,7 @@ NumericMatrix iter_r12(Rcpp::DataFrame dat, int window){
   return r12_matrix;
 }
 
+
 // [[Rcpp::export]]
 NumericMatrix iter_r13(Rcpp::DataFrame dat, int window){
   NumericVector r13 = dat["r13"];
@@ -170,6 +177,8 @@ NumericMatrix iter_r13(Rcpp::DataFrame dat, int window){
   }
   return r13_matrix;
 }
+
+
 
 // [[Rcpp::export]]
 NumericMatrix iter_r_on(Rcpp::DataFrame dat, int window){
@@ -184,6 +193,7 @@ NumericMatrix iter_r_on(Rcpp::DataFrame dat, int window){
   return r_on_matrix;
 }
 
+
 // [[Rcpp::export]]
 NumericMatrix iter_r_onfh(Rcpp::DataFrame dat, int window){
   NumericVector r_onfh = dat["r_onfh"];
@@ -196,6 +206,7 @@ NumericMatrix iter_r_onfh(Rcpp::DataFrame dat, int window){
   }
   return r_onfh_matrix;
 }
+
 
 // [[Rcpp::export]]
 NumericMatrix iter_r13_lag(Rcpp::DataFrame dat, int window){
@@ -232,5 +243,551 @@ NumericVector cumRet_benchmark(NumericVector returns){
     cumRet(i) = results-1;
   }
   return cumRet;
+}
+
+
+//threshold for overnight return
+// [[Rcpp::export]]
+NumericMatrix mr1_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r1 = dat["r1"];
+  NumericVector ron = dat["r_on"];
+  int n = r1.length();
+  int m = thres.length();
+  NumericMatrix mr1(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr1(i,j)=r1[i];
+      }else mr1(i,j)=999;
+    }
+  }
+  return mr1;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr1_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r1 = dat["r1"];
+  NumericVector ron = dat["r_on"];
+  int n = r1.length();
+  int m = thres.length();
+  NumericMatrix mr1(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr1(i,j)=r1[i];
+      }else mr1(i,j)=999;
+    }
+  }
+  return mr1;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr2_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r2 = dat["r2"];
+  NumericVector ron = dat["r_on"];
+  int n = r2.length();
+  int m = thres.length();
+  NumericMatrix mr2(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr2(i,j)=r2[i];
+      }else mr2(i,j)=999;
+    }
+  }
+  return mr2;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr2_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r2 = dat["r2"];
+  NumericVector ron = dat["r_on"];
+  int n = r2.length();
+  int m = thres.length();
+  NumericMatrix mr2(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr2(i,j)=r2[i];
+      }else mr2(i,j)=999;
+    }
+  }
+  return mr2;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr3_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r3 = dat["r3"];
+  NumericVector ron = dat["r_on"];
+  int n = r3.length();
+  int m = thres.length();
+  NumericMatrix mr3(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr3(i,j)=r3[i];
+      }else mr3(i,j)=999;
+    }
+  }
+  return mr3;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr3_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r3 = dat["r3"];
+  NumericVector ron = dat["r_on"];
+  int n = r3.length();
+  int m = thres.length();
+  NumericMatrix mr3(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr3(i,j)=r3[i];
+      }else mr3(i,j)=999;
+    }
+  }
+  return mr3;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr4_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r4 = dat["r4"];
+  NumericVector ron = dat["r_on"];
+  int n = r4.length();
+  int m = thres.length();
+  NumericMatrix mr4(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr4(i,j)=r4[i];
+      }else mr4(i,j)=999;
+    }
+  }
+  return mr4;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr4_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r4 = dat["r4"];
+  NumericVector ron = dat["r_on"];
+  int n = r4.length();
+  int m = thres.length();
+  NumericMatrix mr4(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr4(i,j)=r4[i];
+      }else mr4(i,j)=999;
+    }
+  }
+  return mr4;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr5_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r5 = dat["r5"];
+  NumericVector ron = dat["r_on"];
+  int n = r5.length();
+  int m = thres.length();
+  NumericMatrix mr5(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr5(i,j)=r5[i];
+      }else mr5(i,j)=999;
+    }
+  }
+  return mr5;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr5_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r5 = dat["r5"];
+  NumericVector ron = dat["r_on"];
+  int n = r5.length();
+  int m = thres.length();
+  NumericMatrix mr5(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr5(i,j)=r5[i];
+      }else mr5(i,j)=999;
+    }
+  }
+  return mr5;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr6_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r6 = dat["r6"];
+  NumericVector ron = dat["r_on"];
+  int n = r6.length();
+  int m = thres.length();
+  NumericMatrix mr6(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr6(i,j)=r6[i];
+      }else mr6(i,j)=999;
+    }
+  }
+  return mr6;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr6_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r6 = dat["r6"];
+  NumericVector ron = dat["r_on"];
+  int n = r6.length();
+  int m = thres.length();
+  NumericMatrix mr6(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr6(i,j)=r6[i];
+      }else mr6(i,j)=999;
+    }
+  }
+  return mr6;
+}
+
+
+// [[Rcpp::export]]
+NumericMatrix mr7_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r7 = dat["r7"];
+  NumericVector ron = dat["r_on"];
+  int n = r7.length();
+  int m = thres.length();
+  NumericMatrix mr7(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr7(i,j)=r7[i];
+      }else mr7(i,j)=999;
+    }
+  }
+  return mr7;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr7_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r7 = dat["r7"];
+  NumericVector ron = dat["r_on"];
+  int n = r7.length();
+  int m = thres.length();
+  NumericMatrix mr7(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr7(i,j)=r7[i];
+      }else mr7(i,j)=999;
+    }
+  }
+  return mr7;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr8_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r8 = dat["r8"];
+  NumericVector ron = dat["r_on"];
+  int n = r8.length();
+  int m = thres.length();
+  NumericMatrix mr8(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr8(i,j)=r8[i];
+      }else mr8(i,j)=999;
+    }
+  }
+  return mr8;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr8_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r8 = dat["r8"];
+  NumericVector ron = dat["r_on"];
+  int n = r8.length();
+  int m = thres.length();
+  NumericMatrix mr8(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr8(i,j)=r8[i];
+      }else mr8(i,j)=999;
+    }
+  }
+  return mr8;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr9_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r9 = dat["r9"];
+  NumericVector ron = dat["r_on"];
+  int n = r9.length();
+  int m = thres.length();
+  NumericMatrix mr9(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr9(i,j)=r9[i];
+      }else mr9(i,j)=999;
+    }
+  }
+  return mr9;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr9_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r9 = dat["r9"];
+  NumericVector ron = dat["r_on"];
+  int n = r9.length();
+  int m = thres.length();
+  NumericMatrix mr9(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr9(i,j)=r9[i];
+      }else mr9(i,j)=999;
+    }
+  }
+  return mr9;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr10_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r10 = dat["r10"];
+  NumericVector ron = dat["r_on"];
+  int n = r10.length();
+  int m = thres.length();
+  NumericMatrix mr10(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr10(i,j)=r10[i];
+      }else mr10(i,j)=999;
+    }
+  }
+  return mr10;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr10_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r10 = dat["r10"];
+  NumericVector ron = dat["r_on"];
+  int n = r10.length();
+  int m = thres.length();
+  NumericMatrix mr10(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr10(i,j)=r10[i];
+      }else mr10(i,j)=999;
+    }
+  }
+  return mr10;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr11_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r11 = dat["r11"];
+  NumericVector ron = dat["r_on"];
+  int n = r11.length();
+  int m = thres.length();
+  NumericMatrix mr11(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr11(i,j)=r11[i];
+      }else mr11(i,j)=999;
+    }
+  }
+  return mr11;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr11_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r11 = dat["r11"];
+  NumericVector ron = dat["r_on"];
+  int n = r11.length();
+  int m = thres.length();
+  NumericMatrix mr11(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr11(i,j)=r11[i];
+      }else mr11(i,j)=999;
+    }
+  }
+  return mr11;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr12_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r12 = dat["r12"];
+  NumericVector ron = dat["r_on"];
+  int n = r12.length();
+  int m = thres.length();
+  NumericMatrix mr12(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr12(i,j)=r12[i];
+      }else mr12(i,j)=999;
+    }
+  }
+  return mr12;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr12_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r12 = dat["r12"];
+  NumericVector ron = dat["r_on"];
+  int n = r12.length();
+  int m = thres.length();
+  NumericMatrix mr12(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr12(i,j)=r12[i];
+      }else mr12(i,j)=999;
+    }
+  }
+  return mr12;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr13_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r13 = dat["r13"];
+  NumericVector ron = dat["r_on"];
+  int n = r13.length();
+  int m = thres.length();
+  NumericMatrix mr13(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr13(i,j)=r13[i];
+      }else mr13(i,j)=999;
+    }
+  }
+  return mr13;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr13_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r13 = dat["r13"];
+  NumericVector ron = dat["r_on"];
+  int n = r13.length();
+  int m = thres.length();
+  NumericMatrix mr13(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr13(i,j)=r13[i];
+      }else mr13(i,j)=999;
+    }
+  }
+  return mr13;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr_on_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r_on = dat["r_on"];
+  NumericVector ron = dat["r_on"];
+  int n = r_on.length();
+  int m = thres.length();
+  NumericMatrix mr_on(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr_on(i,j)=r_on[i];
+      }else mr_on(i,j)=999;
+    }
+  }
+  return mr_on;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr_on_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r_on = dat["r_on"];
+  NumericVector ron = dat["r_on"];
+  int n = r_on.length();
+  int m = thres.length();
+  NumericMatrix mr_on(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr_on(i,j)=r_on[i];
+      }else mr_on(i,j)=999;
+    }
+  }
+  return mr_on;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr_onfh_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r_onfh = dat["r_onfh"];
+  NumericVector ron = dat["r_on"];
+  int n = r_onfh.length();
+  int m = thres.length();
+  NumericMatrix mr_onfh(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr_onfh(i,j)=r_onfh[i];
+      }else mr_onfh(i,j)=999;
+    }
+  }
+  return mr_onfh;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr_onfh_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r_onfh = dat["r_onfh"];
+  NumericVector ron = dat["r_on"];
+  int n = r_onfh.length();
+  int m = thres.length();
+  NumericMatrix mr_onfh(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr_onfh(i,j)=r_onfh[i];
+      }else mr_onfh(i,j)=999;
+    }
+  }
+  return mr_onfh;
+}
+// [[Rcpp::export]]
+NumericMatrix mr13_lag_thresholdL(DataFrame dat, NumericVector thres){
+  NumericVector r13_lag = dat["r13_lag"];
+  NumericVector ron = dat["r_on"];
+  int n = r13_lag.length();
+  int m = thres.length();
+  NumericMatrix mr13_lag(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]<thres[j]){
+        mr13_lag(i,j)=r13_lag[i];
+      }else mr13_lag(i,j)=999;
+    }
+  }
+  return mr13_lag;
+}
+
+// [[Rcpp::export]]
+NumericMatrix mr13_lag_thresholdGeq(DataFrame dat, NumericVector thres){
+  NumericVector r13_lag = dat["r13_lag"];
+  NumericVector ron = dat["r_on"];
+  int n = r13_lag.length();
+  int m = thres.length();
+  NumericMatrix mr13_lag(n,m);
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      if(ron[i]>=thres[j]){
+        mr13_lag(i,j)=r13_lag[i];
+      }else mr13_lag(i,j)=999;
+    }
+  }
+  return mr13_lag;
 }
 
