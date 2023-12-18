4ceca07c9a8148e3e55d2fe948f386cc7af7d187
LuZhang907 <63075429+LuZhang907@users.noreply.github.com>
2023-12-18 11:51:34 -0600

    regime regression via various overnight returns

---
+++ Code/regime_split_by_overnightReturn.R
@@ -0,0 +1,88 @@
+rm(list = setdiff(ls(),lsf.str()))
+library(data.table)
+library(Rcpp)
+
+sourceCpp("./Code/iteration_matrix_generating.cpp", verbose = F, rebuild = T)
+
+dat <- fread("./Data/returns_SPY.csv")
+dat <- data.frame(dat)
+dat <- dat %>% drop_na()
+head(dat)
+dim(dat)
+
+hist(dat$r_on)
+
+min_ <- min(dat$r_on)
+max_ <- max(dat$r_on)
+
+thres <- round(seq(min_, max_, 0.01),4)
+n<- length(thres)
+
+mr1L <- mr1_thresholdL(dat,thres)
+mr1Geq <- mr1_thresholdGeq(dat,thres)
+
+mr2L <- mr2_thresholdL(dat,thres)
+mr2Geq <- mr2_thresholdGeq(dat,thres)
+
+mr3L <- mr3_thresholdL(dat,thres)
+mr3Geq <- mr3_thresholdGeq(dat,thres)
+
+mr4L <- mr4_thresholdL(dat,thres)
+mr4Geq <- mr4_thresholdGeq(dat,thres)
+
+mr4L <- mr4_thresholdL(dat,thres)
+mr4Geq <- mr4_thresholdGeq(dat,thres)
+
+mr5L <- mr5_thresholdL(dat,thres)
+mr5Geq <- mr5_thresholdGeq(dat,thres)
+
+mr6L <- mr6_thresholdL(dat,thres)
+mr6Geq <- mr6_thresholdGeq(dat,thres)
+
+mr7L <- mr7_thresholdL(dat,thres)
+mr7Geq <- mr7_thresholdGeq(dat,thres)
+
+mr8L <- mr8_thresholdL(dat,thres)
+mr8Geq <- mr8_thresholdGeq(dat,thres)
+
+mr9L <- mr9_thresholdL(dat,thres)
+mr9Geq <- mr9_thresholdGeq(dat,thres)
+
+mr10L <- mr10_thresholdL(dat,thres)
+mr10Geq <- mr10_thresholdGeq(dat,thres)
+
+mr11L <- mr11_thresholdL(dat,thres)
+mr11Geq <- mr11_thresholdGeq(dat,thres)
+
+mr12L <- mr12_thresholdL(dat,thres)
+mr12Geq <- mr12_thresholdGeq(dat,thres)
+
+mr13L <- mr13_thresholdL(dat,thres)
+mr13Geq <- mr13_thresholdGeq(dat,thres)
+
+mr13lagL <- mr13_lag_thresholdL(dat,thres)
+mr13lagGeq <- mr13_lag_thresholdGeq(dat,thres)
+
+mronL <- mr_on_thresholdL(dat,thres)
+mronGeq <- mr_on_thresholdGeq(dat,thres)
+
+my_lmsGeq <- lapply(1:n, function(x) lm(mr13Geq[,x][!mr13Geq[,x]==999] ~ 
+                                       mr1Geq[,x][!mr1Geq[,x]==999]+
+                                       mr2Geq[,x][!mr2Geq[,x]==999]+
+                                       mr3Geq[,x][!mr3Geq[,x]==999]+
+                                       mr4Geq[,x][!mr4Geq[,x]==999]+
+                                       mr5Geq[,x][!mr5Geq[,x]==999]+
+                                       mr6Geq[,x][!mr6Geq[,x]==999]+
+                                       mr7Geq[,x][!mr7Geq[,x]==999]+
+                                       mr8Geq[,x][!mr8Geq[,x]==999]+   
+                                       mr9Geq[,x][!mr9Geq[,x]==999]+
+                                       mr10Geq[,x][!mr10Geq[,x]==999]+
+                                       mr11Geq[,x][!mr11Geq[,x]==999]+
+                                       mr12Geq[,x][!mr12Geq[,x]==999]+
+                                       mr13lagGeq[,x][!mr13lagGeq[,x]==999]+ 
+                                       mronGeq[,x][!mronGeq[,x]==999]
+                                     ))
+
+summaries <- lapply(my_lmsGeq, summary)
+
+
